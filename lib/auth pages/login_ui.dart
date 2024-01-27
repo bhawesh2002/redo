a95b48d30d73d9251e09fe1777a409efcdf2e037
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-01-27 22:05:58 +0530

    create actionButton widget

--- lib/auth pages/login_ui.dart
+++ lib/auth pages/login_ui.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:redo/assets/colors/colors.dart';
 import 'package:redo/widgets.dart';
 
 class LoginPage extends StatelessWidget {
@@ -9,9 +10,31 @@ class LoginPage extends StatelessWidget {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [redoLogo(context, 0.3)],
+          children: [
+            redoLogo(context, 0.3),
+          ],
         ),
       ),
     );
   }
+}
+
+Widget actionButton(BuildContext context, String actionName) {
+  double containerWidth = MediaQuery.of(context).size.width * 0.6;
+  double containerHeight = MediaQuery.of(context).size.height * 0.06;
+  return Container(
+    decoration: BoxDecoration(
+      color: AppColor.primaryColor,
+      borderRadius: BorderRadius.circular(containerWidth * 0.05),
+    ),
+    width: containerWidth,
+    height: containerHeight,
+    child: Center(
+      child: Text(
+        actionName,
+        style: const TextStyle(
+            fontSize: 18, color: Colors.white, fontWeight: FontWeight.bold),
+      ),
+    ),
+  );
 }
