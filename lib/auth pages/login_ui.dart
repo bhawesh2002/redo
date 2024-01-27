8599e1ac9f4101a8fc6c827933dbc65c8b562d33
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-01-27 20:49:22 +0530

    add redoLogo to the LoginPage

--- lib/auth pages/login_ui.dart
+++ lib/auth pages/login_ui.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:redo/widgets.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [redoLogo(context, 0.3)],
+        ),
+      ),
+    );
   }
 }
