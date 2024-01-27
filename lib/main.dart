e05ac5f3d10edd0cc72f6c492d8af95507775e22
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-01-27 09:32:39 +0530

    create Redo class

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,14 @@
+import 'package:flutter/material.dart';
 
+void main() => runApp(const Redo());
+
+class Redo extends StatelessWidget {
+  const Redo({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Redo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+    );
+  }
+}
