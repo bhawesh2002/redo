077858ed736032749a45e216e00ac01b825e51b9
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-01-28 00:14:55 +0530

    add SafeArea

--- lib/auth pages/login_ui.dart
+++ lib/auth pages/login_ui.dart
@@ -7,52 +7,54 @@ class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            redoLogo(context, 0.3),
-            const Padding(padding: EdgeInsets.all(30)),
-            actionButton(context, "Login"),
-            const Padding(padding: EdgeInsets.all(30)),
-            seperation("OR"),
-            const Padding(padding: EdgeInsets.all(30)),
-            const Text(
-              "Login Using",
-              style: TextStyle(fontSize: 12),
-            ),
-            const Padding(padding: EdgeInsets.all(10)),
-            authAlternatives(
-              context,
-              "Google",
-              imgPath: "lib/assets/images/google-color-icon.svg",
-            ),
-            const Padding(padding: EdgeInsets.all(30)),
-            const Text(
-              "Don't have an Account",
-              style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500),
-            ),
-            GestureDetector(
-              onTap: () {
-                debugPrint("Opening Signup Page");
-              },
-              child: Container(
-                color: Colors.transparent,
-                width: MediaQuery.of(context).size.width * 0.3,
-                height: MediaQuery.of(context).size.height * 0.05,
-                child: const Center(
-                  child: Text(
-                    "SignUp",
-                    style: TextStyle(
-                        color: AppColor.primaryColor,
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold),
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              redoLogo(context, 0.3),
+              const Padding(padding: EdgeInsets.all(30)),
+              actionButton(context, "Login"),
+              const Padding(padding: EdgeInsets.all(30)),
+              seperation("OR"),
+              const Padding(padding: EdgeInsets.all(30)),
+              const Text(
+                "Login Using",
+                style: TextStyle(fontSize: 12),
+              ),
+              const Padding(padding: EdgeInsets.all(10)),
+              authAlternatives(
+                context,
+                "Google",
+                imgPath: "lib/assets/images/google-color-icon.svg",
+              ),
+              const Padding(padding: EdgeInsets.all(30)),
+              const Text(
+                "Don't have an Account",
+                style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500),
+              ),
+              GestureDetector(
+                onTap: () {
+                  debugPrint("Opening Signup Page");
+                },
+                child: Container(
+                  color: Colors.transparent,
+                  width: MediaQuery.of(context).size.width * 0.3,
+                  height: MediaQuery.of(context).size.height * 0.05,
+                  child: const Center(
+                    child: Text(
+                      "SignUp",
+                      style: TextStyle(
+                          color: AppColor.primaryColor,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold),
+                    ),
                   ),
                 ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
