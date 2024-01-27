85dc3606b157224f5eaf0d1bf84db6a4bfb69452
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-01-28 00:10:53 +0530

    create auth_form class

---
+++ lib/auth pages/auth_form.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class AuthForm extends StatefulWidget {
+  const AuthForm({super.key});
+  @override
+  State<AuthForm> createState() => _AuthFormState();
+}
+
+class _AuthFormState extends State<AuthForm> {
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            const Text(
+              "Login",
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+            const Padding(padding: EdgeInsets.all(20)),
+            TextFormField(
+              controller: emailController,
+              autofocus: true,
+              decoration: InputDecoration(
+                labelText: "Enter Your Email",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+              ),
+              keyboardType: TextInputType.emailAddress,
+            ),
+            const Padding(padding: EdgeInsets.all(10)),
+            TextFormField(
+              controller: passwordController,
+              autofocus: true,
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: "Enter Your Password",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+              ),
+              keyboardType: TextInputType.emailAddress,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
