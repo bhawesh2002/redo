ff83c43e5206ec1653f5cab64c82b3d2284d929e
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-01-28 21:53:47 +0530

    convert Home to StatefulWidget

--- lib/home.dart
+++ lib/home.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:redo/widgets.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
