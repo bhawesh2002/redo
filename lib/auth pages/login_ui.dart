39d1b06f542de9e7380715c3bd1a7ed4a96f03d9
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-01-28 11:42:56 +0530

    remove unused variable

--- lib/auth pages/login_ui.dart
+++ lib/auth pages/login_ui.dart
@@ -9,7 +9,6 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
-    double width = MediaQuery.of(context).size.width;
     return SafeArea(
       child: Scaffold(
         resizeToAvoidBottomInset: false,
