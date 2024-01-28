18134d0639b9346fb8940b91718f1b100c1f3636
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-01-29 00:02:46 +0530

    deserialize JSON data and display a list view

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:redo/widgets.dart';
+import 'todo.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -10,14 +11,41 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          leading: redoLogo(context, 0.2),
-          title: const Text("Redo"),
-        ),
-      ),
+          appBar: AppBar(
+            leading: redoLogo(context, 0.2),
+            title: const Text("Redo"),
+          ),
+          body: FutureBuilder<List<Todo>>(
+            future: getTasks(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const CircularProgressIndicator();
+              } else if (snapshot.hasError) {
+                return Text('Error: ${snapshot.error}');
+              } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                return const Text('No tasks available.');
+              } else {
+                return ListView.builder(
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (context, index) {
+                    Todo todo = snapshot.data![index];
+                    return ListTile(
+                      title: Text(todo.title),
+                      subtitle: Text(todo.description),
+                    );
+                  },
+                );
+              }
+            },
+          )),
     );
   }
 }
