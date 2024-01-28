3d3a6f49c698b5d55f549ecd25187e2117ed859e
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-01-28 19:12:32 +0530

    create class Todo with required fields

--- lib/todo.dart
+++ lib/todo.dart
@@ -1,1 +1,21 @@
+import 'package:flutter/material.dart';
 
+class Todo {
+  final String id;
+  final String title;
+  final String description;
+  final DateTime date;
+  final TimeOfDay startTime;
+  final TimeOfDay endTime;
+  final bool isComleted;
+
+  Todo({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.date,
+    required this.startTime,
+    required this.endTime,
+    required this.isComleted,
+  });
+}
