7c3540cd858e0df825cb1fa2c452d784082ed72c
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-01-29 00:02:01 +0530

    remove date, startTime and endTime

--- lib/todo.dart
+++ lib/todo.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'dart:convert';
 
@@ -6,29 +5,19 @@ class Todo {
   final String id;
   final String title;
   final String description;
-  final DateTime date;
-  final TimeOfDay startTime;
-  final TimeOfDay endTime;
   final bool isComleted;
 
   Todo({
     required this.id,
     required this.title,
     required this.description,
-    required this.date,
-    required this.startTime,
-    required this.endTime,
     required this.isComleted,
   });
-
   factory Todo.fromJson(Map<String, dynamic> json) {
     return Todo(
       id: json['id'],
       title: json['title'],
       description: json['description'],
-      date: DateTime.parse(json['date']),
-      startTime: TimeOfDay.fromDateTime(DateTime.parse(json['startTime'])),
-      endTime: TimeOfDay.fromDateTime(DateTime.parse(json['endTime'])),
       isComleted: json['isComleted'] ?? false,
     );
   }
@@ -38,32 +27,19 @@ class Todo {
       'id': id,
       'title': title,
       'description': description,
-      'date': date.toIso8601String(),
-      'startTime': DateTime(
-        date.year,
-        date.month,
-        date.day,
-        startTime.hour,
-        startTime.minute,
-      ).toIso8601String(),
-      'endTime': DateTime(
-        date.year,
-        date.month,
-        date.day,
-        endTime.hour,
-        endTime.minute,
-      ).toIso8601String(),
       'isCompleted': isComleted,
     };
   }
 }
 
-Future<List<Todo>> loadTasks(String jsonPath) async {
-  return rootBundle.loadString(jsonPath).then(
-    (jsonData) {
-      List<dynamic> jsonList = json.decode(jsonData);
-      List<Todo> tasks = jsonList.map((json) => Todo.fromJson(json)).toList();
-      return tasks;
-    },
-  );
+Future<List<Todo>> getTasks() async {
+  final jsonData = await rootBundle.loadString('lib/assets/tasks/tasks.json');
+  final tasks = loadTasks(jsonData);
+  return tasks;
+}
+
+List<Todo> loadTasks(String jsonData) {
+  List<dynamic> jsonList = json.decode(jsonData);
+  List<Todo> tasks = jsonList.map((json) => Todo.fromJson(json)).toList();
+  return tasks;
 }
