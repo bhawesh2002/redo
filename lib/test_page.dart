91db21f6338fef48630c7c9f5bd9c4a28f36bdf2
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-01-29 12:27:35 +0530

    create TestPage class

--- lib/test_page.dart
+++ lib/test_page.dart
@@ -1,1 +1,13 @@
+import 'package:flutter/material.dart';
 
+class TestPage extends StatelessWidget {
+  const TestPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Test Page"),
+      ),
+    );
+  }
+}
