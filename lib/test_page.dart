780c253293fa913f384a2cc15437ddaa15ecac2e
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-01-30 09:42:18 +0530

    add app bar to the TestPage

--- lib/test_page.dart
+++ lib/test_page.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:redo/assets/colors/colors.dart';
 import 'package:redo/todo.dart';
+import 'package:redo/widgets.dart';
 
 class TestPage extends StatelessWidget {
   const TestPage({super.key});
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Redo"),
+        leading: redoLogo(context, 0.2),
+        toolbarHeight: MediaQuery.of(context).size.height * 0.08,
+        actions: [
+          CircleAvatar(
+            radius: MediaQuery.of(context).size.height * 0.04,
+          )
+        ],
+      ),
+      body: const Center(
         child: TaskWidget(),
       ),
     );
