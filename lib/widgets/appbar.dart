959a755c2c6ff847f247e17d0d6e4e625a58d1e2
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-02-04 11:53:36 +0530

    layout widgets inside TodoAppBar

--- lib/widgets/appbar.dart
+++ lib/widgets/appbar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:redo/widgets/redologo.dart';
 
 class TodoAppBar extends StatelessWidget implements PreferredSizeWidget {
   final BuildContext context;
@@ -6,8 +7,48 @@ class TodoAppBar extends StatelessWidget implements PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.amber,
+    double appBarHeight = MediaQuery.of(context).size.height * 0.1;
+    return SizedBox(
+      height: appBarHeight,
+      child: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: MediaQuery.of(context).size.width * 0.05,
+              ),
+              child: Row(
+                children: [
+                  redoLogo(context, appBarHeight * 0.0015, false),
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: MediaQuery.of(context).size.width * 0.02,
+                    ),
+                  ),
+                  Text(
+                    "Redo",
+                    style: TextStyle(
+                      fontSize: appBarHeight * 0.3,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: MediaQuery.of(context).size.width * 0.05,
+              ),
+              child: CircleAvatar(
+                radius: appBarHeight * 0.4,
+                backgroundColor: Colors.white,
+                child: Image.asset('lib/assets/images/avataaars.png'),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 
