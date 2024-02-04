2382557ac44c78d1162579ed7b4597b1f86c4e10
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-02-04 13:04:09 +0530

    cretae TodoBottomAppBar stateful widget

---
+++ lib/widgets/bottomAppBar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TodoBottomAppBar extends StatefulWidget {
+  const TodoBottomAppBar({super.key});
+
+  @override
+  State<TodoBottomAppBar> createState() => _TodoBottomAppBarState();
+}
+
+class _TodoBottomAppBarState extends State<TodoBottomAppBar> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
