bfd58c411f119846c955beedaee05e9db46efbe4
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-02-04 12:28:07 +0530

    move contents of TestPage to HomePage and refactoring changes

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:redo/widgets/appbar.dart';
+import 'package:redo/widgets/task_widget.dart';
 import 'package:redo/todo.dart';
-import 'package:redo/widgets/redologo.dart';
+import 'package:redo/widgets/taskfilter.dart';
+import 'package:redo/widgets/tasktile.dart';
 
-class Home extends StatefulWidget {
-  const Home({super.key});
+List<String> filterLabel = [
+  "Upcoming",
+  "All Tasks",
+  "Completed",
+  "Skipped",
+];
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<Home> createState() => _HomeState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _HomeState extends State<Home> {
+class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
@@ -19,38 +29,93 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-          appBar: AppBar(
-            leading: redoLogo(context, 0.2, true),
-            title: const Text("Redo"),
-          ),
-          body: FutureBuilder<List<Todo>>(
-            future: getTasks(),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const CircularProgressIndicator();
-              } else if (snapshot.hasError) {
-                return Text('Error: ${snapshot.error}');
-              } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                return const Text('No tasks available.');
-              } else {
-                return ListView.builder(
-                  itemCount: snapshot.data!.length,
-                  itemBuilder: (context, index) {
-                    Todo todo = snapshot.data![index];
-                    return ListTile(
-                      title: Text(todo.title),
-                      subtitle: Text(todo.description),
-                      onTap: () {
-                        debugPrint(
-                            "Start Date: ${todo.startDate}\tStart Time: ${todo.startTime}\n"
-                            "End Date: ${todo.endDate}\t\tEnd Time: ${todo.endTime}");
-                      },
-                    );
-                  },
-                );
-              }
-            },
-          )),
+        appBar: TodoAppBar(context: context),
+        body: FutureBuilder(
+          future: getTasks(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            } else if (snapshot.hasError) {
+              return Text("Error: ${snapshot.error}");
+            } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+              return const Text("No tasks Available");
+            } else {
+              List<Todo> taskList = snapshot.data ?? [];
+              return Padding(
+                padding: EdgeInsets.symmetric(
+                    vertical: MediaQuery.of(context).size.height * 0.01,
+                    horizontal: MediaQuery.of(context).size.width * 0.04),
+                child: LayoutBuilder(
+                    builder: (context, BoxConstraints constraints) {
+                  return Column(
+                    children: [
+                      const Align(
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                          "Ongoing,",
+                          style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                            vertical: constraints.maxHeight * 0.01),
+                      ),
+                      TaskWidget(
+                        index: 2,
+                        taskList: taskList,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                            vertical: constraints.maxHeight * 0.01),
+                      ),
+                      const Align(
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                          "Tasks",
+                          style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                            vertical: constraints.maxHeight * 0.005),
+                      ),
+                      TaskFilter(constraints: constraints),
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                            vertical: constraints.maxHeight * 0.01),
+                      ),
+                      Expanded(
+                        child: SizedBox(
+                          height: constraints.maxHeight * 0.5,
+                          child: ListView.builder(
+                              itemCount: taskList.length,
+                              itemBuilder: (context, index) {
+                                return Padding(
+                                  padding: EdgeInsets.symmetric(
+                                      vertical: constraints.maxHeight * 0.01,
+                                      horizontal: constraints.maxWidth * 0.02),
+                                  child: TaskTile(
+                                    index: index,
+                                    taskList: taskList,
+                                  ),
+                                );
+                              }),
+                        ),
+                      )
+                    ],
+                  );
+                }),
+              );
+            }
+          },
+        ),
+      ),
     );
   }
 }
--- lib/screens/test_page.dart
+++ lib/screens/test_page.dart
@@ -1,16 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:redo/widgets/appbar.dart';
-import 'package:redo/widgets/task_widget.dart';
-import 'package:redo/todo.dart';
-import 'package:redo/widgets/taskfilter.dart';
-import 'package:redo/widgets/tasktile.dart';
-
-List<String> filterLabel = [
-  "Upcoming",
-  "All Tasks",
-  "Completed",
-  "Skipped",
-];
 
 class TestPage extends StatefulWidget {
   const TestPage({super.key});
@@ -27,94 +15,9 @@ class _TestPageState extends State<TestPage> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: TodoAppBar(context: context),
-        body: FutureBuilder(
-          future: getTasks(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const CircularProgressIndicator();
-            } else if (snapshot.hasError) {
-              return Text("Error: ${snapshot.error}");
-            } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-              return const Text("No tasks Available");
-            } else {
-              List<Todo> taskList = snapshot.data ?? [];
-              return Padding(
-                padding: EdgeInsets.symmetric(
-                    vertical: MediaQuery.of(context).size.height * 0.01,
-                    horizontal: MediaQuery.of(context).size.width * 0.04),
-                child: LayoutBuilder(
-                    builder: (context, BoxConstraints constraints) {
-                  return Column(
-                    children: [
-                      const Align(
-                        alignment: Alignment.topLeft,
-                        child: Text(
-                          "Ongoing,",
-                          style: TextStyle(
-                            fontSize: 25,
-                            fontWeight: FontWeight.w700,
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.symmetric(
-                            vertical: constraints.maxHeight * 0.01),
-                      ),
-                      TaskWidget(
-                        index: 2,
-                        taskList: taskList,
-                      ),
-                      Padding(
-                        padding: EdgeInsets.symmetric(
-                            vertical: constraints.maxHeight * 0.01),
-                      ),
-                      const Align(
-                        alignment: Alignment.topLeft,
-                        child: Text(
-                          "Tasks",
-                          style: TextStyle(
-                            fontSize: 25,
-                            fontWeight: FontWeight.w700,
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.symmetric(
-                            vertical: constraints.maxHeight * 0.005),
-                      ),
-                      TaskFilter(constraints: constraints),
-                      Padding(
-                        padding: EdgeInsets.symmetric(
-                            vertical: constraints.maxHeight * 0.01),
-                      ),
-                      Expanded(
-                        child: SizedBox(
-                          height: constraints.maxHeight * 0.5,
-                          child: ListView.builder(
-                              itemCount: taskList.length,
-                              itemBuilder: (context, index) {
-                                return Padding(
-                                  padding: EdgeInsets.symmetric(
-                                      vertical: constraints.maxHeight * 0.01,
-                                      horizontal: constraints.maxWidth * 0.02),
-                                  child: TaskTile(
-                                    index: index,
-                                    taskList: taskList,
-                                  ),
-                                );
-                              }),
-                        ),
-                      )
-                    ],
-                  );
-                }),
-              );
-            }
-          },
-        ),
+    return const Scaffold(
+      body: Center(
+        child: Text("TestPage"),
       ),
     );
   }
