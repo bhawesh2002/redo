2648dd85c8e651da890ec05ef907a118b15c526e
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-02-04 11:05:39 +0530

    create TodoAppBar Stateless Widget implementing PreferredSizeWidget

---
+++ lib/widgets/appbar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TodoAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const TodoAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+  @override
+  // TODO: implement preferredSize
+  Size get preferredSize => throw UnimplementedError();
+}
