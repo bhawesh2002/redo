7a60bf9641e6ad190b8d2c9873e9ab4eb5a9dffa
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-02-05 11:47:04 +0530

    move code to Tasks StatefulWidget and make refactoring changes

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:redo/assets/colors/colors.dart';
 import 'package:redo/screens/stats.dart';
+import 'package:redo/screens/tasks.dart';
 import 'package:redo/widgets/appbar.dart';
 import 'package:redo/widgets/todo_bottom_app_bar_item.dart';
-import 'package:redo/widgets/task_widget.dart';
 import 'package:redo/todo.dart';
-import 'package:redo/widgets/taskfilter.dart';
-import 'package:redo/widgets/tasktile.dart';
 
 List<String> filterLabel = [
   "Upcoming",
@@ -51,77 +49,8 @@ class _HomePageState extends State<HomePage> {
               if (_selectedIndex == BottomNavItem.Stats.index) {
                 return const Stats();
               } else {
-                return Padding(
-                  padding: EdgeInsets.symmetric(
-                      vertical: MediaQuery.of(context).size.height * 0.01,
-                      horizontal: MediaQuery.of(context).size.width * 0.04),
-                  child: LayoutBuilder(
-                      builder: (context, BoxConstraints constraints) {
-                    return Column(
-                      children: [
-                        const Align(
-                          alignment: Alignment.topLeft,
-                          child: Text(
-                            "Ongoing,",
-                            style: TextStyle(
-                              fontSize: 25,
-                              fontWeight: FontWeight.w700,
-                            ),
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.symmetric(
-                              vertical: constraints.maxHeight * 0.01),
-                        ),
-                        TaskWidget(
-                          index: 2,
-                          taskList: taskList,
-                        ),
-                        Padding(
-                          padding: EdgeInsets.symmetric(
-                              vertical: constraints.maxHeight * 0.01),
-                        ),
-                        const Align(
-                          alignment: Alignment.topLeft,
-                          child: Text(
-                            "Tasks",
-                            style: TextStyle(
-                              fontSize: 25,
-                              fontWeight: FontWeight.w700,
-                            ),
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.symmetric(
-                              vertical: constraints.maxHeight * 0.005),
-                        ),
-                        TaskFilter(constraints: constraints),
-                        Padding(
-                          padding: EdgeInsets.symmetric(
-                              vertical: constraints.maxHeight * 0.01),
-                        ),
-                        Expanded(
-                          child: SizedBox(
-                            height: constraints.maxHeight * 0.5,
-                            child: ListView.builder(
-                                itemCount: taskList.length,
-                                itemBuilder: (context, index) {
-                                  return Padding(
-                                    padding: EdgeInsets.symmetric(
-                                        vertical: constraints.maxHeight * 0.01,
-                                        horizontal:
-                                            constraints.maxWidth * 0.02),
-                                    child: TaskTile(
-                                      index: index,
-                                      taskList: taskList,
-                                    ),
-                                  );
-                                }),
-                          ),
-                        )
-                      ],
-                    );
-                  }),
+                return Tasks(
+                  taskList: taskList,
                 );
               }
             }
