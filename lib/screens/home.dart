2337b08c52fdd7a0407eec98a20ca2efb1238ecc
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-02-06 17:52:00 +0530

    Layout the widgets inside the BottomSheet

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -108,18 +108,225 @@ class _HomePageState extends State<HomePage> {
           splashColor: Colors.teal.shade50,
           onPressed: () {
             showModalBottomSheet(
-                context: context,
-                builder: (BuildContext context) {
-                  return Container(
-                    width: MediaQuery.of(context).size.width,
-                    decoration:
-                        BoxDecoration(borderRadius: BorderRadius.circular(10)),
-                    child: const Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [Text("Create New Task")],
+              showDragHandle: true,
+              context: context,
+              builder: (BuildContext context) {
+                return Container(
+                  width: MediaQuery.of(context).size.width,
+                  decoration:
+                      BoxDecoration(borderRadius: BorderRadius.circular(10)),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: MediaQuery.of(context).size.width * 0.05,
                     ),
-                  );
-                });
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Align(
+                          alignment: Alignment.topLeft,
+                          child: Text(
+                            "Create New Task",
+                            style: TextStyle(
+                              fontSize:
+                                  MediaQuery.of(context).size.width * 0.06,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width,
+                          height: MediaQuery.of(context).size.height * 0.015,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(
+                            horizontal:
+                                MediaQuery.of(context).size.width * 0.01,
+                            vertical: MediaQuery.of(context).size.height * 0.01,
+                          ),
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                              labelText: "Title",
+                              focusedBorder: OutlineInputBorder(
+                                  borderSide: const BorderSide(
+                                    width: 3,
+                                    color: AppColor.primaryColor,
+                                  ),
+                                  borderRadius: BorderRadius.circular(10)),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  width: 2,
+                                  color: Colors.teal.shade100,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(
+                            horizontal:
+                                MediaQuery.of(context).size.width * 0.01,
+                            vertical: MediaQuery.of(context).size.height * 0.01,
+                          ),
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                              labelText: "Description",
+                              focusedBorder: OutlineInputBorder(
+                                  borderSide: const BorderSide(
+                                    width: 3,
+                                    color: AppColor.primaryColor,
+                                  ),
+                                  borderRadius: BorderRadius.circular(10)),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  width: 2,
+                                  color: Colors.teal.shade100,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width,
+                          height: MediaQuery.of(context).size.height * 0.015,
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            showDatePicker(
+                              context: context,
+                              firstDate: DateTime.now(),
+                              lastDate: DateTime(2050),
+                            );
+                          },
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                children: [
+                                  Icon(
+                                    Icons.calendar_month_rounded,
+                                    size: MediaQuery.of(context).size.width *
+                                        0.06,
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.symmetric(
+                                      horizontal:
+                                          MediaQuery.of(context).size.width *
+                                              0.01,
+                                    ),
+                                  ),
+                                  Text(
+                                    "Task Date",
+                                    style: TextStyle(
+                                      fontSize:
+                                          MediaQuery.of(context).size.width *
+                                              0.04,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              const Text("Select Date"),
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width,
+                          height: MediaQuery.of(context).size.height * 0.02,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            GestureDetector(
+                              onTap: () {
+                                showTimePicker(
+                                    context: context,
+                                    initialTime: TimeOfDay.now());
+                              },
+                              child: Row(
+                                children: [
+                                  Icon(
+                                    Icons.access_time,
+                                    size: MediaQuery.of(context).size.width *
+                                        0.06,
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.symmetric(
+                                      horizontal:
+                                          MediaQuery.of(context).size.width *
+                                              0.01,
+                                    ),
+                                  ),
+                                  Text(
+                                    "Start Time",
+                                    style: TextStyle(
+                                      fontSize:
+                                          MediaQuery.of(context).size.width *
+                                              0.04,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            GestureDetector(
+                              onTap: () {
+                                showTimePicker(
+                                    context: context,
+                                    initialTime: TimeOfDay.now());
+                              },
+                              child: Row(
+                                children: [
+                                  Icon(
+                                    Icons.access_time_filled,
+                                    size: MediaQuery.of(context).size.width *
+                                        0.06,
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.symmetric(
+                                      horizontal:
+                                          MediaQuery.of(context).size.width *
+                                              0.01,
+                                    ),
+                                  ),
+                                  Text(
+                                    "End Time",
+                                    style: TextStyle(
+                                      fontSize:
+                                          MediaQuery.of(context).size.width *
+                                              0.04,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                        const Expanded(child: SizedBox()),
+                        Flexible(
+                          child: Container(
+                            width: MediaQuery.of(context).size.width * 0.4,
+                            height: MediaQuery.of(context).size.height * 0.05,
+                            decoration: BoxDecoration(
+                              color: AppColor.primaryColor,
+                              borderRadius: BorderRadius.circular(5),
+                            ),
+                            child: Center(
+                              child: Text(
+                                "Create",
+                                style: TextStyle(
+                                  fontSize:
+                                      MediaQuery.of(context).size.width * 0.05,
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                );
+              },
+            );
           },
           child: const Icon(
             Icons.add,
