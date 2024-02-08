79d30c3e9c9f88f3aac2d8d82077623e323dece8
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-02-08 17:13:12 +0530

    Use LayoutBuilder

--- lib/widgets/create_task_bottom_sheet.dart
+++ lib/widgets/create_task_bottom_sheet.dart
@@ -29,141 +29,147 @@ class _CreateBottomTaskSheetState extends State<CreateBottomTaskSheet> {
           padding: EdgeInsets.symmetric(
             horizontal: MediaQuery.of(context).size.width * 0.05,
           ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Align(
-                alignment: Alignment.topLeft,
-                child: Text(
-                  "Create New Task",
-                  style: TextStyle(
-                    fontSize: MediaQuery.of(context).size.width * 0.06,
-                    fontWeight: FontWeight.bold,
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      "Create New Task",
+                      style: TextStyle(
+                        fontSize: MediaQuery.of(context).size.width * 0.06,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
                   ),
-                ),
-              ),
-              SizedBox(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height * 0.015,
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: MediaQuery.of(context).size.width * 0.01,
-                  vertical: MediaQuery.of(context).size.height * 0.01,
-                ),
-                child: TextFormField(
-                  controller: _titleController,
-                  decoration: textFieldDecoration(labelText: "Title"),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: MediaQuery.of(context).size.width * 0.01,
-                  vertical: MediaQuery.of(context).size.height * 0.01,
-                ),
-                child: TextFormField(
-                  controller: _descriptionController,
-                  decoration: textFieldDecoration(labelText: "Description"),
-                ),
-              ),
-              SizedBox(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height * 0.015,
-              ),
-              GestureDetector(
-                onTap: () {
-                  showDatePicker(
-                    context: context,
-                    firstDate: DateTime.now(),
-                    lastDate: DateTime(2050),
-                  );
-                },
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height * 0.015,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: MediaQuery.of(context).size.width * 0.01,
+                      vertical: MediaQuery.of(context).size.height * 0.01,
+                    ),
+                    child: TextFormField(
+                      controller: _titleController,
+                      decoration: textFieldDecoration(labelText: "Title"),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: MediaQuery.of(context).size.width * 0.01,
+                      vertical: MediaQuery.of(context).size.height * 0.01,
+                    ),
+                    child: TextFormField(
+                      controller: _descriptionController,
+                      decoration: textFieldDecoration(labelText: "Description"),
+                    ),
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height * 0.015,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      showDatePicker(
+                        context: context,
+                        firstDate: DateTime.now(),
+                        lastDate: DateTime(2050),
+                      );
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Icon(
-                          Icons.calendar_month_rounded,
-                          size: MediaQuery.of(context).size.width * 0.06,
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.calendar_month_rounded,
+                              size: MediaQuery.of(context).size.width * 0.06,
+                            ),
+                            Padding(
+                              padding: EdgeInsets.symmetric(
+                                horizontal:
+                                    MediaQuery.of(context).size.width * 0.01,
+                              ),
+                            ),
+                            Text(
+                              "Task Date",
+                              style: TextStyle(
+                                fontSize:
+                                    MediaQuery.of(context).size.width * 0.04,
+                              ),
+                            ),
+                          ],
                         ),
-                        Padding(
-                          padding: EdgeInsets.symmetric(
-                            horizontal:
-                                MediaQuery.of(context).size.width * 0.01,
-                          ),
-                        ),
-                        Text(
-                          "Task Date",
-                          style: TextStyle(
-                            fontSize: MediaQuery.of(context).size.width * 0.04,
-                          ),
-                        ),
+                        const Text("Select Date"),
                       ],
                     ),
-                    const Text("Select Date"),
-                  ],
-                ),
-              ),
-              SizedBox(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height * 0.02,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      showTimePicker(
-                          context: context, initialTime: TimeOfDay.now());
-                    },
-                    child: const TimeSelector(
-                      label: "Start Time",
-                      icon: Icons.access_time_rounded,
-                    ),
                   ),
-                  GestureDetector(
-                    onTap: () {
-                      showTimePicker(
-                          context: context, initialTime: TimeOfDay.now());
-                    },
-                    child: const TimeSelector(
-                      label: "End Time",
-                      icon: Icons.access_time_filled_rounded,
-                    ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height * 0.02,
                   ),
-                ],
-              ),
-              const Expanded(child: SizedBox()),
-              Flexible(
-                child: GestureDetector(
-                  onTap: () {
-                    setInputData();
-                    debugPrint("Title: $_title");
-                    debugPrint("Description: $_description");
-                    Navigator.of(context).pop();
-                  },
-                  child: Container(
-                    width: MediaQuery.of(context).size.width * 0.4,
-                    height: MediaQuery.of(context).size.height * 0.05,
-                    decoration: BoxDecoration(
-                      color: AppColor.primaryColor,
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                    child: Center(
-                      child: Text(
-                        "Create",
-                        style: TextStyle(
-                          fontSize: MediaQuery.of(context).size.width * 0.05,
-                          fontWeight: FontWeight.w600,
-                          color: Colors.white,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          showTimePicker(
+                              context: context, initialTime: TimeOfDay.now());
+                        },
+                        child: const TimeSelector(
+                          label: "Start Time",
+                          icon: Icons.access_time_rounded,
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          showTimePicker(
+                              context: context, initialTime: TimeOfDay.now());
+                        },
+                        child: const TimeSelector(
+                          label: "End Time",
+                          icon: Icons.access_time_filled_rounded,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const Expanded(child: SizedBox()),
+                  Flexible(
+                    child: GestureDetector(
+                      onTap: () {
+                        setInputData();
+                        debugPrint("Title: $_title");
+                        debugPrint("Description: $_description");
+                        Navigator.of(context).pop();
+                      },
+                      child: Container(
+                        width: MediaQuery.of(context).size.width * 0.4,
+                        height: MediaQuery.of(context).size.height * 0.05,
+                        decoration: BoxDecoration(
+                          color: AppColor.primaryColor,
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                        child: Center(
+                          child: Text(
+                            "Create",
+                            style: TextStyle(
+                              fontSize:
+                                  MediaQuery.of(context).size.width * 0.05,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.white,
+                            ),
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-              ),
-            ],
+                ],
+              );
+            },
           ),
         );
       },
