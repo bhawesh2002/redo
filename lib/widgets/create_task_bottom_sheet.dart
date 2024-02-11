109b1ca3cf13f8f17a018946785d926eedaec4a9
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-02-11 10:12:12 +0530

    Use a form to validate data entered by user

--- lib/widgets/create_task_bottom_sheet.dart
+++ lib/widgets/create_task_bottom_sheet.dart
@@ -10,6 +10,7 @@ class CreateBottomTaskSheet extends StatefulWidget {
 }
 
 class _CreateBottomTaskSheetState extends State<CreateBottomTaskSheet> {
+  final _formKey = GlobalKey<FormState>();
   final TextEditingController _titleController = TextEditingController();
   final TextEditingController _descriptionController = TextEditingController();
   String? _title;
@@ -29,156 +30,163 @@ class _CreateBottomTaskSheetState extends State<CreateBottomTaskSheet> {
     return BottomSheet(
       onClosing: () {},
       builder: (BuildContext context) {
-        return Padding(
-          padding: EdgeInsets.symmetric(
-            horizontal: MediaQuery.of(context).size.width * 0.05,
-          ),
-          child: LayoutBuilder(
-            builder: (context, constraints) {
-              return Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Align(
-                    alignment: Alignment.topLeft,
-                    child: Text(
-                      "Create New Task",
-                      style: TextStyle(
-                        fontSize: constraints.maxWidth * 0.06,
-                        fontWeight: FontWeight.bold,
+        return Form(
+          key: _formKey,
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: MediaQuery.of(context).size.width * 0.05,
+            ),
+            child: LayoutBuilder(
+              builder: (context, constraints) {
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        "Create New Task",
+                        style: TextStyle(
+                          fontSize: constraints.maxWidth * 0.06,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    width: constraints.maxWidth,
-                    height: constraints.maxHeight * 0.04,
-                  ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: constraints.maxWidth * 0.01,
-                      vertical: constraints.maxHeight * 0.02,
+                    SizedBox(
+                      width: constraints.maxWidth,
+                      height: constraints.maxHeight * 0.04,
                     ),
-                    child: TextFormField(
-                        controller: _titleController,
-                        textInputAction: TextInputAction.next,
-                        autovalidateMode: AutovalidateMode.onUserInteraction,
-                        style: const TextStyle(fontWeight: FontWeight.bold),
-                        validator: (value) {
-                          return value!.isEmpty ? "Title is required" : null;
-                        },
-                        decoration: textFieldDecoration(labelText: "Title")),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: constraints.maxWidth * 0.01,
-                      vertical: constraints.maxHeight * 0.02,
-                    ),
-                    child: TextFormField(
-                      controller: _descriptionController,
-                      decoration: textFieldDecoration(labelText: "Description"),
-                    ),
-                  ),
-                  SizedBox(
-                    width: constraints.maxWidth,
-                    height: constraints.maxHeight * 0.04,
-                  ),
-                  GestureDetector(
-                    onTap: () async {
-                      DateTime? date = await showDatePicker(
-                        context: context,
-                        initialDate: _selectedDate,
-                        firstDate: DateTime.now(),
-                        lastDate: DateTime(2050),
-                      );
-                      setState(() {
-                        if (date != null) {
-                          _selectedDate = date;
-                        }
-                      });
-                    },
-                    child: Padding(
-                        padding: EdgeInsets.symmetric(
-                            vertical: constraints.maxHeight * 0.02),
-                        child: DateSelector(
-                            date: _selectedDate, constraints: constraints)),
-                  ),
-                  SizedBox(
-                    width: constraints.maxWidth,
-                    height: constraints.maxHeight * 0.04,
-                  ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(
-                        vertical: constraints.maxHeight * 0.02),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        GestureDetector(
-                          onTap: () async {
-                            TimeOfDay? time = await showTimePicker(
-                                context: context, initialTime: TimeOfDay.now());
-                            setState(() {
-                              if (time != null) {
-                                _startTime = time.toString().substring(10, 15);
-                              }
-                            });
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: constraints.maxWidth * 0.01,
+                        vertical: constraints.maxHeight * 0.02,
+                      ),
+                      child: TextFormField(
+                          controller: _titleController,
+                          textInputAction: TextInputAction.next,
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          style: const TextStyle(fontWeight: FontWeight.bold),
+                          validator: (value) {
+                            return value!.isEmpty ? "Title is required" : null;
                           },
-                          child: TimeSelector(
-                            time: _startTime,
-                            icon: Icons.access_time_rounded,
-                            constraints: constraints,
-                          ),
-                        ),
-                        GestureDetector(
-                          onTap: () async {
-                            TimeOfDay? time = await showTimePicker(
-                                context: context, initialTime: TimeOfDay.now());
-                            setState(() {
-                              if (time != null) {
-                                _endTime = time.toString().substring(10, 15);
-                              }
-                            });
-                          },
-                          child: TimeSelector(
-                            time: _endTime,
-                            icon: Icons.access_time_filled_rounded,
-                            constraints: constraints,
-                          ),
-                        ),
-                      ],
+                          decoration: textFieldDecoration(labelText: "Title")),
                     ),
-                  ),
-                  const Expanded(child: SizedBox()),
-                  Flexible(
-                    child: GestureDetector(
-                      onTap: () {
-                        setInputData();
-                        debugPrint("Title: $_title");
-                        debugPrint("Description: $_description");
-                        Navigator.of(context).pop();
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: constraints.maxWidth * 0.01,
+                        vertical: constraints.maxHeight * 0.02,
+                      ),
+                      child: TextFormField(
+                        controller: _descriptionController,
+                        decoration:
+                            textFieldDecoration(labelText: "Description"),
+                      ),
+                    ),
+                    SizedBox(
+                      width: constraints.maxWidth,
+                      height: constraints.maxHeight * 0.04,
+                    ),
+                    GestureDetector(
+                      onTap: () async {
+                        DateTime? date = await showDatePicker(
+                          context: context,
+                          initialDate: _selectedDate,
+                          firstDate: DateTime.now(),
+                          lastDate: DateTime(2050),
+                        );
+                        setState(() {
+                          if (date != null) {
+                            _selectedDate = date;
+                          }
+                        });
                       },
-                      child: Container(
-                        width: MediaQuery.of(context).size.width * 0.4,
-                        height: MediaQuery.of(context).size.height * 0.05,
-                        decoration: BoxDecoration(
-                          color: AppColor.primaryColor,
-                          borderRadius: BorderRadius.circular(5),
-                        ),
-                        child: Center(
-                          child: Text(
-                            "Create",
-                            style: TextStyle(
-                              fontSize:
-                                  MediaQuery.of(context).size.width * 0.05,
-                              fontWeight: FontWeight.w600,
-                              color: Colors.white,
+                      child: Padding(
+                          padding: EdgeInsets.symmetric(
+                              vertical: constraints.maxHeight * 0.02),
+                          child: DateSelector(
+                              date: _selectedDate, constraints: constraints)),
+                    ),
+                    SizedBox(
+                      width: constraints.maxWidth,
+                      height: constraints.maxHeight * 0.04,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                          vertical: constraints.maxHeight * 0.02),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          GestureDetector(
+                            onTap: () async {
+                              TimeOfDay? time = await showTimePicker(
+                                  context: context,
+                                  initialTime: TimeOfDay.now());
+                              setState(() {
+                                if (time != null) {
+                                  _startTime =
+                                      time.toString().substring(10, 15);
+                                }
+                              });
+                            },
+                            child: TimeSelector(
+                              time: _startTime,
+                              icon: Icons.access_time_rounded,
+                              constraints: constraints,
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () async {
+                              TimeOfDay? time = await showTimePicker(
+                                  context: context,
+                                  initialTime: TimeOfDay.now());
+                              setState(() {
+                                if (time != null) {
+                                  _endTime = time.toString().substring(10, 15);
+                                }
+                              });
+                            },
+                            child: TimeSelector(
+                              time: _endTime,
+                              icon: Icons.access_time_filled_rounded,
+                              constraints: constraints,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const Expanded(child: SizedBox()),
+                    Flexible(
+                      child: GestureDetector(
+                        onTap: () {
+                          setInputData();
+                          debugPrint("Title: $_title");
+                          debugPrint("Description: $_description");
+                          Navigator.of(context).pop();
+                        },
+                        child: Container(
+                          width: MediaQuery.of(context).size.width * 0.4,
+                          height: MediaQuery.of(context).size.height * 0.05,
+                          decoration: BoxDecoration(
+                            color: AppColor.primaryColor,
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          child: Center(
+                            child: Text(
+                              "Create",
+                              style: TextStyle(
+                                fontSize:
+                                    MediaQuery.of(context).size.width * 0.05,
+                                fontWeight: FontWeight.w600,
+                                color: Colors.white,
+                              ),
                             ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                ],
-              );
-            },
+                  ],
+                );
+              },
+            ),
           ),
         );
       },
