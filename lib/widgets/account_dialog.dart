78a8e451602eb8c32ede395759fb27f844ed950a
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-02-17 16:17:37 +0530

    dismiss accountDialog beform navigating to Settings Page

--- lib/widgets/account_dialog.dart
+++ lib/widgets/account_dialog.dart
@@ -95,6 +95,7 @@ Future<void> accountDialog(BuildContext context, String name) {
               acDialogButton(
                 context,
                 onTap: () {
+                  Navigator.of(context).pop();
                   Navigator.of(context).push(MaterialPageRoute(
                       builder: (context) => const SettingsPage()));
                 },
