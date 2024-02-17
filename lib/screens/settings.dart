2cf27e2cadf5a5c59b02d50f78b1ab13b8127edb
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-02-17 15:42:57 +0530

    create SettingsPage widget

---
+++ lib/screens/settings.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Settings"),
+        ),
+      ),
+    );
+  }
+}
