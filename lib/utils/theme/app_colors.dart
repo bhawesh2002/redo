3fa74b594a1e9a5f687ea71951dd36513ca1793b
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-07-30 18:25:42 +0530

    refactor: Add AppColors class for managing color constants

---
+++ lib/utils/theme/app_colors.dart
@@ -0,0 +1,37 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color white = Color(0xffffffff);
+  static const Color black = Color(0xff000000);
+
+  // Primary Color
+  static const Map<String, Color> emrald = {
+    '50': Color(0xffeffef6),
+    '100': Color(0xffd9ffee),
+    '200': Color(0xffb5fdde),
+    '300': Color(0xff7bfac4),
+    '400': Color(0xff3beda3),
+    '500': Color(0xff11d483),
+    '600': Color(0xff07b26b),
+    '700': Color(0xff0a8b56),
+    '800': Color(0xff0e6d47),
+    '900': Color(0xff0d5a3d),
+    '950': Color(0xff013220),
+  };
+  //Neutral Color
+  static const Map<String, Color> silver = {
+    '50': Color(0xfff7f7f7),
+    '100': Color(0xfff0f0f0),
+    '200': Color(0xffe3e3e3),
+    '300': Color(0xffd1d1d1),
+    '400': Color(0xffc2c2c2),
+    '500': Color(0xffaaaaaa),
+    '600': Color(0xff969696),
+    '700': Color(0xff818181),
+    '800': Color(0xff6a6a6a),
+    '900': Color(0xff585858),
+    '950': Color(0xff333333),
+    '1000': Color(0xFF242424),
+    '1050': Color(0xFF1A1A1A),
+  };
+}
